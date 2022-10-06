d56480dfcc5b0b38767b8ad82f3bf65400851bba
Timofey <alekseev-timof@mail.ru>
2022-10-06 18:16:40 +0300

    Final!

--- second_task.py
+++ second_task.py
@@ -35,6 +35,30 @@ def multiplication(first_matrix):
     return new_matrix
 
 
+def rank(matrix):
+    minor_data = []
+    a1, a2, a3 = matrix[0][0], matrix[1][0], matrix[2][0]
+    b1, b2, b3 = matrix[0][1], matrix[1][1], matrix[2][1]
+    c1, c2, c3 = matrix[0][2], matrix[1][2], matrix[2][2]
+    new_a1 = c2 * b3 - b2 * c3
+    new_a2 = c1 * b3 - b1 * c3
+    new_a3 = c1 * b2 - b1 * c2
+    new_b1 = c2 * a3 - a2 * c3
+    new_b2 = c1 * a3 - a1 * c3
+    new_b3 = c1 * a2 - a1 * c2
+    new_c1 = b2 * a3 - a2 * b3
+    new_c2 = b1 * a3 - a1 * b3
+    new_c3 = b1 * a2 - a1 * b2
+    main_minor = a1 * b2 * c3 + a2 * b3 * c1 + a3 * b1 * c2 - a3 * b2 * c1 - a2 * b1 * c3 - a1 * b3 * c2
+    minor_data.append((new_a1, new_b1, new_c1, new_a2, new_b2, new_c2, new_a3, new_b3, new_c3))
+    if max(minor_data[0]) == 0:
+        max_minor = 1
+    elif max(minor_data[0]) > main_minor:
+        max_minor = 2
+    else:
+        max_minor = 3
+    return max_minor
+
 
 matrix = []
 print("Введите матрицу построчно. Когда вы закончите вводить данные, нажмите дважды клавишу Enter")
@@ -44,4 +68,7 @@ while True:
         break
     else:
         matrix.append(list(map(int, elem.split())))
+
+print(transp(matrix))
+print(rank(matrix))
 print(multiplication(matrix))
